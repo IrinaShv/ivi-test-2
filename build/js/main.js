414b7bd2b6e35bc0d121aaf040de63ef715ea854
Irina <i.shvaych@gmail.com>
2023-10-09 14:45:30 +0300

    Upload the task source files

---
+++ build/js/main.js
@@ -0,0 +1,54 @@
+// DOM элементы
+const elementContainer = document.querySelector('.container');
+const timerElement = document.querySelector('.diamond');
+const timerTextElement = document.querySelector('.diamond__text');
+
+// переменная, хранящая интервал
+let timerLoop;
+
+// Обработчики событий :
+// * запуск таймера при клике на кнопку
+timerElement.addEventListener('click', initCountdown, { once: true });
+
+// * меняем цвет фона при наведении на кнопку
+['mouseover', 'touchstart'].forEach(event => timerElement.addEventListener(event, () => elementContainer.classList.add('container_highlight')));
+['mouseout', 'touchend'].forEach(event => timerElement.addEventListener(event, () => elementContainer.classList.remove('container_highlight')));
+
+// функция, инициирующая первый запуск таймера
+function initCountdown() { startCountdown(timerElement, timerTextElement) }
+
+// функция, запускающая таймер на кнопке и обновляющая значение в ней
+function startCountdown(buttonEl, textEl) {
+    let currentCount = 1;
+    textEl.innerHTML = currentCount;
+    buttonEl.disabled = true;
+
+    const countdown = setInterval(() => {
+        currentCount++;
+        if (currentCount > 3) {
+            clearInterval(countdown);
+            textEl.innerHTML = 'Go!';
+            buttonEl.disabled = false;
+
+            startLoop(3);
+        } else {
+            textEl.innerHTML = currentCount;
+        }
+    }, 1000);
+}
+
+// функция, управляющая интервалом ( останавливаем / запускаем )
+function startLoop(count) {
+    clearInterval(timerLoop);
+
+    if (!count) return;
+
+    let loopCount = count;
+    timerLoop = setInterval(() => {
+        loopCount--;
+        if (loopCount < 0) {
+            clearInterval(timerLoop);
+            startCountdown(timerElement, timerTextElement)
+        }
+    }, 1000);
+}
---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+'use strict';
+
+import pkg from 'gulp';
+const { gulp, src, dest, parallel, series, watch } = pkg;
+
+import pug from 'gulp-pug';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
+import svgmin from 'gulp-svgmin';
+import imagemin from 'gulp-imagemin';
+import imageminJpegRecompress from 'imagemin-jpeg-recompress';
+import browserSync from 'browser-sync';
+
+const paths = {
+  dev: {
+    html: 'dev/pug/index.pug',
+    img: 'dev/img/*.{*, !svg}',
+    svg: 'dev/img/svg/*',
+    css: 'dev/sass/**/*',
+    js: 'dev/js/**/*',
+  },
+  build: {
+    html: 'build/',
+    svg: 'build/img/svg',
+    css: 'build/css',
+    img: 'build/img',
+    js: 'build/js',
+  },
+  watch: {
+    html: 'dev/pug/**/*'
+  },
+  serv: {
+    html: 'build',
+  },
+};
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: paths.serv.html
+    }
+  })
+}
+
+function html() {
+  return src(paths.dev.html)
+    .pipe(pug({ pretty: true }))
+    .pipe(dest(paths.build.html))
+}
+
+function css() {
+  return src(paths.dev.css)
+    .pipe(sass())
+    .pipe(dest(paths.build.css))
+    .pipe(browserSync.stream())
+}
+
+function js() {
+  return src(paths.dev.js)
+    .pipe(dest(paths.build.js))
+}
+
+function svg() {
+  return src(paths.dev.svg)
+    .pipe(svgmin())
+    .pipe(dest(paths.build.svg))
+    .pipe(browserSync.stream())
+}
+
+function img() {
+  return src(paths.dev.img)
+    .pipe(imagemin([
+      imageminJpegRecompress({
+        loops: 6,
+        min: 70,
+        max: 85,
+        quality: 'high'
+      })
+    ]))
+    .pipe(dest(paths.build.img))
+    .pipe(browserSync.stream());
+}
+
+function startwatch() {
+  watch(paths.watch.html, html);
+  watch(paths.dev.css, css);
+  watch(paths.dev.img, img);
+  watch(paths.dev.svg, svg);
+  watch(paths.dev.js, js);
+  watch([paths.watch.html, paths.dev.js]).on('change', browserSync.reload);
+}
+
+export default series(html, css, svg, img, parallel(browsersync, startwatch));
